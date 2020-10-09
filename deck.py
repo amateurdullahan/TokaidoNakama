4d5ef5b607186ce7a5096e86664c362aa10a3ad6
Dullahan <zabrak12@gmail.com>
2020-10-09 13:56:56 -0500

    fixed typo, basically empty deck file

---
+++ deck.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""deck objects"""
+
+class Card:
+        def __init__(self)
+
+class Deck(Card):
+        def __init__(self)
--- screen.py
+++ screen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import pygames
+import pygame
 import sys
 
 pygame.init()
@@ -8,4 +8,4 @@ size = width, height = 1024, 768
 white = 255, 255, 255
 screen = pygame.display.set_mode(size)
 screen.fill(white)
-pygames.display.flip()
+pygame.display.flip()
