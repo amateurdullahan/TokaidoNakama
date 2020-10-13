e3dbd0abc1def435582f8ad1a3745b480ffe62eb
Dullahan <zabrak12@gmail.com>
2020-10-13 13:52:57 -0500

    updated Meal and Souvenir with cost attr, modified deck init

--- deck.py
+++ deck.py
@@ -48,15 +48,59 @@ class Encounter(Card):
             super().__init__(type, point_value, name)
 
 class Meal(Card):
-        def __init__(self, type="", point_value=0, name=""):
-            super().__init__(type, point_value, name)
+        def __init__(self, type="", point_value=0, name="", cost=0):
+            self.type = type
+            self.point_value = point_value
+            self.name = name
+            self.cost = cost
+
+        @property
+        def type(self):
+            """comment"""
+            return self.__type
+
+        @type.setter
+        def type(self, string):
+            """comment"""
+            self.__type = string
+
+        @property
+        def point_value(self):
+            """comment"""
+            return self.__point_value
+
+        @point_value.setter
+        def point_value(self, value):
+            """comment"""
+            self.__point_value = value
+
+        @property
+        def name(self):
+            """comment"""
+            return self.__name
+
+        @name.setter
+        def name(self, string):
+            """comment"""
+            self.__name = string
+
+        @property
+        def cost(self):
+            """comment"""
+            return self.__cost
+
+        @cost.setter
+        def cost(self, value):
+            """comment"""
+            self.__cost = value
 
 class Souvenir(Card):
-        def __init__(self, type="", point_value=0, name="", subtype=""):
+        def __init__(self, type="", point_value=0, name="", subtype="", cost=0):
             self.type = type
             self.point_value = point_value
             self.name = name
             self.subtype = subtype
+            self.cost = cost
 
         @property
         def type(self):
@@ -97,6 +141,16 @@ class Souvenir(Card):
         @subtype.setter
         def subtype(self, string):
             self.__subtype = string
+
+        @property
+        def cost(self):
+            """comment"""
+            return self.__cost
+
+        @cost.setter
+        def cost(self, value):
+            """comment"""
+            self.__cost = value
 
 class Deck():
         def __init__(self, number_of_cards=0, owner="", card_list=[]):
