8c4f043e4ea4b1ace3875a20a6bbc17c111ad5a4
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-10-16 16:12:36 -0500

    loc functions except encounter and inn

--- location.py
+++ location.py
@@ -40,31 +40,65 @@ def Farm_Loc(Player):
 
 def Panorama_Paddy_Loc(Player):
     """paddy location function"""
-    Player.pano_paddy += 1
-    Player.points += Player.pano_paddy
-    """achievment check"""
+    if player.pano_paddy < 3:
+        Player.pano_paddy += 1
+        Player.points += Player.pano_paddy
+        """achievment check"""
 
 def Panorama_Mountain_Loc(Player):
     """mountain location function"""
-    Player.pano_mt += 1
-    Player.points += Player.pano_mt
-    """achievment check"""
+    if Player.pano_mt < 4:
+        Player.pano_mt += 1
+        Player.points += Player.pano_mt
+        """achievment check"""
 
 def Panorama_Sea_Loc(Player):
     """sea location function"""
-    Player.pano_sea += 1
-    Player.points += Player.pano_sea
-    """achievment check"""
+    if Player.pano_sea < 5:
+        Player.pano_sea += 1
+        Player.points += Player.pano_sea
+        """achievment check"""
 
 def Temple_Loc(Player):
     """temple location function TBW"""
-    Player.coins -= 1 """actual number will be player input"""
+    """Ask player for num of coins"""
+    Player.coins -= num
+    Player.score += num
+    """ temple bonus check"""
 
 def Souvenir_Shop_Loc(Player):
     """souvenirshop location function TBW"""
+    """ask player which cards bought, do this three times"""
+    if discarded:
+        """move card to discard deck"""
+    else:
+        Player.coins -= bought.cost
+        if Player.sv_type0 is None:
+           Player.sv_type0 = bought.subtype
+        elif Player.sv_type0 == bought.subtype:
+            Player.score += 1
+        elif Player.sv_type1 is None:
+            Player.sv_type1 = bought.subtype
+        elif Player.sv_type1 == bought.subtype:
+            Player.score += 3
+        elif Player.sv_type2 is None:
+            Player.sv_type2 = bought.subtype
+        elif Player.sv_type2 == bought.subtype:
+            Player.score += 5
+        elif Player.sv_type3 is None:
+            Player.sv_type3 = bought.subtype
+        elif Player.sv_type3 == bought.subtype:
+            Player.score += 7
+        """move card to player deck"""
+        """check collector acheviement"""
+    
 
 def Hot_Spring_Loc(Player):
     """hot spring location function TBW"""
+    """ask player if 2 or 3 pts"""
+    Player.score += pts
+    """check Bather achievement"""
+    """move card to player deck"""
 
 def Inn_Loc(Player):
     """inn location function TBW"""
--- player.py
+++ player.py
@@ -2,7 +2,7 @@
 """player objects"""
 
 class Player():
-        def __init__(self, color="", score=0, coins=7, board_space=0, pano_paddy=0, pano_mt=0, pano_sea=0):
+        def __init__(self, color="", score=0, coins=7, board_space=0, pano_paddy=0, pano_mt=0, pano_sea=0, sv_type0="", sv_type1="", sv_type2="", sv_type3=""):
             """initializing the players"""
             self.color = color
             self.score = score
@@ -11,6 +11,10 @@ class Player():
             self.pano_paddy = pano_paddy
             self.pano_mt = pano_mt
             self.pano_sea = pano_sea
+            self.sv_type0 = sv_type0
+            self.sv_type1 = sv_type1
+            self.sv_type2 = sv_type2
+            self.sv_type3 = sv_type3
 
         @property
         def color(self):
@@ -81,6 +85,46 @@ class Player():
         def pano_sea(self, value):
             """comment"""
             self.__pano_sea = value
+
+        @property
+        def sv_type0(self):
+            """comment"""
+            return self.__sv_type0
+
+        @sv_type0.setter
+        def sv_type0(self, string):
+            """comment"""
+            self.__sv_type0 = string
+
+        @property
+        def sv_type1(self):
+            """comment"""
+            return self.__sv_type1
+
+        @sv_type1.setter
+        def sv_type1(self, string):
+            """comment"""
+            self.__sv_type1 = string
+
+        @property
+        def sv_type2(self):
+            """comment"""
+            return self.__sv_type2
+
+        @sv_type2.setter
+        def sv_type2(self, string):
+            """comment"""
+            self.__sv_type2 = string
+
+        @property
+        def sv_type3(self):
+            """comment"""
+            return self.__sv_type3
+
+        @sv_type3.setter
+        def sv_type3(self, string):
+            """comment"""
+            self.__sv_type3 = string
 
 
 class Achievements():
